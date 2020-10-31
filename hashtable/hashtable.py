9edd9203fdcebb6f27d27253164d88eafd89d342
Jarren <jarrenmcampos@gmail.com>
2020-10-30 18:32:12 -0700

    First portion of project

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -21,8 +21,8 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
-
+        self.table = [None] * capacity
+        self.capacity = capacity
 
     def get_num_slots(self):
         """
@@ -34,7 +34,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return len(self.table)
 
 
     def get_load_factor(self):
@@ -62,7 +62,10 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        hash = 5381
+        for x in key:
+            hash = (( hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
 
     def hash_index(self, key):
@@ -70,7 +73,6 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -81,8 +83,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
-
+        self.table[self.hash_index(key)] = value
 
     def delete(self, key):
         """
@@ -92,7 +93,10 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        value = self.table[self.hash_index(key)]
+        if value == None:
+            print('Value is already None')
+        self.table[self.hash_index(key)] = None
 
 
     def get(self, key):
@@ -103,7 +107,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return self.table[self.hash_index(key)]
 
 
     def resize(self, new_capacity):
